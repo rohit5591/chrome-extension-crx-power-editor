9b7bebb2f5919cd53f990282b41a9127fbfd4ba3
Rohit Nagare <rohitnagare@hotmail.com>
2022-10-08 18:11:05 +0530

    WIP

--- src/scripts/init-repo.js
+++ src/scripts/init-repo.js
@@ -4,15 +4,11 @@ const path = 'editors'
 const fs = require("fs");
 if (!fs.existsSync(path)) {
     fs.mkdirSync(path);
-    shell.cd(path)
-    shell.exec('git clone https://github.com/codemirror/dev.git');
+}
+shell.cd(path)
+if (!fs.existsSync('monaco-editor')) {
     shell.exec('git clone https://github.com/microsoft/monaco-editor.git');
-} else {
-    shell.cd(path);
-    if (!fs.existsSync('monaco-editor')) {
-        shell.exec('git clone https://github.com/microsoft/monaco-editor.git');
-    }
-    if (!fs.existsSync('dev')) {
-        shell.exec('git clone https://github.com/codemirror/dev.git');
-    }
+}
+if (!fs.existsSync('dev')) {
+    shell.exec('git clone https://github.com/codemirror/dev.git');
 }
