028cc55664a221ec8ba7225d20ba4394f2194963
Rohit Nagare <rohitnagare@hotmail.com>
2022-10-08 18:07:22 +0530

    WIP

--- src/scripts/init-repo.js
+++ src/scripts/init-repo.js
@@ -1,1 +1,18 @@
 console.log("Init Repo");
+const shell = require('shelljs')
+const path = 'editors'
+const fs = require("fs");
+if (!fs.existsSync(path)) {
+    fs.mkdirSync(path);
+    shell.cd(path)
+    shell.exec('git clone https://github.com/codemirror/dev.git');
+    shell.exec('git clone https://github.com/microsoft/monaco-editor.git');
+} else {
+    shell.cd(path);
+    if (!fs.existsSync('monaco-editor')) {
+        shell.exec('git clone https://github.com/microsoft/monaco-editor.git');
+    }
+    if (!fs.existsSync('dev')) {
+        shell.exec('git clone https://github.com/codemirror/dev.git');
+    }
+}
