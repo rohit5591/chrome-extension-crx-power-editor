ee8aa6db07010a8169c27c706e1e8ded20dd579c
Rohit Nagare <rohitnagare@hotmail.com>
2022-10-09 19:25:07 +0530

    wip

--- src/scripts/build-extension.js
+++ src/scripts/build-extension.js
@@ -7,35 +7,30 @@ var jsonminify = require("jsonminify");
 fs.removeSync('dist');
 fs.mkdirSync('dist');
 
+//Copy all extension files
+fs.copySync('src/extension/.', 'dist/.');
 //Build VS Code Editor for Chrome Extension
 //Copy built folders/files first
 fs.mkdirSync('dist/vs');
 fs.copySync('editors/vs/monaco-editor/release/min/vs', 'dist/vs');
 fs.copySync('editors/vs/vscode-loader/src/loader.js', 'dist/vs/loader.js');
 //Build VS Themes Function
-let options = [];
-let jsonVars = "function getTheme(theme) {\n switch(theme) {\n";
-const themeList = JSON.parse(fs.readFileSync('editors/vs/monaco-themes/themes/themelist.json'));
-// glob.sync('./themes/*.json').forEach(file => {
-//     const fileObj = path.parse(file);
-//     //console.log(fileObj.name);
-//     options.push(fileObj.name);
-//     const themeName = fileObj.name.replaceAll(/[^a-zA-Z0-9]/g, '');
-//     const fileContents = jsonminify(fs.readFileSync(file, { encoding: 'utf8' }));
-//     jsonVars += "case '" + themeName + "' : {\n ";
-//     jsonVars += "const " + themeName + "= " + fileContents + ";\n";
-//     jsonVars += "monaco.editor.defineTheme(\"" + themeName + "\", " + themeName + ");\n";
-//     jsonVars += "monaco.editor.setTheme(\"" + themeName + "\");\n";
-//     jsonVars += "return theme;";
-//     jsonVars += "}\n ";
-//     //jsonVars += "monaco.editor.defineTheme(\"" + fileObj.name + "\", " + fileObj.name + ");\n";
-//     //monaco.editor.defineTheme('monokai', data);
-// });
-// jsonVars += "\n}\n return theme;\n}\n"
-// //console.log("monaco.editor.defineTheme(\"" + option.replaceAll(/[^a-zA-Z0-9]/g, '') + "\", " + option.replaceAll(/[^a-zA-Z0-9]/g, '') + ");");
-
-// console.log(jsonVars);
-// // console.log("selct dropdowns ======");
-// // options.forEach(option => {
-// //     console.log("<option value=\"" + option.replaceAll(/[^a-zA-Z0-9]/g, '') + "\">" + option + "</option>");
-// // });
+let vsOptions = "";
+let jsonVars = "function getTheme(theme) {\n    switch(theme) {\n";
+const themesPath = 'editors/vs/monaco-themes/themes/';
+const themeList = JSON.parse(fs.readFileSync(themesPath + 'themelist.json'));
+Object.keys(themeList).forEach(key => {
+    const themeKey = key.replaceAll(/[^a-zA-Z0-9]/g, '');;
+    const themeName = themeList[key];
+    const fileContents = jsonminify(fs.readFileSync(`${themesPath}${themeName}.json`, { encoding: 'utf8' }));
+    jsonVars += "        case '" + themeKey + "' : {\n ";
+    jsonVars += "            const " + themeKey + "= " + fileContents + ";\n";
+    jsonVars += "            monaco.editor.defineTheme(\"" + themeKey + "\", " + themeKey + ");\n";
+    jsonVars += "            monaco.editor.setTheme(\"" + themeKey + "\");\n";
+    jsonVars += "            return theme;\n";
+    jsonVars += "        }\n";
+    options += `<option value=\"${themeKey}\">${themeName}</option>\n`;
+});
+jsonVars += "    }\n    return theme;\n}\n"
+console.log(jsonVars);
+console.log(options);
