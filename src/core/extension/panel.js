6bc6fd3372592bc82400a6d961c8e2390e436ff9
Rohit Nagare <rohitnagare@hotmail.com>
2022-10-20 11:19:38 +0530

    wip

--- src/core/extension/panel.js
+++ src/core/extension/panel.js
@@ -75,21 +75,37 @@ const setLabelStatus = (isEnable) => {
 	}
 };
 
+const triggerError = (message) => {
+	$('#txtUrl').addClass('is-invalid');
+	$('.url-error').html(message);
+	var count = 3;
+	var x = setInterval(function () {
+		if (count <= 0) {
+			$('#txtUrl').removeClass('is-invalid');
+			clearInterval(x);
+		}
+		count--;
+	}, 1000);
+};
+
 const getUrl = () => {
 	const url = $('#txtUrl').val();
 	if (isValidHttpUrl(url)) {
-		$('#txtUrl').val('');
-		addUrlSection('new', url);
+		const url = new URL(value);
+		if($('.url-regex').text().indexOf(url) === -1) {
+			$('#txtUrl').val('');
+			addUrlSection('new', url);
+		} else {
+			triggerError('Domain already added in the list');
+		}
 	} else {
+		triggerError('Invalid URL');
 		console.log('invalid url');
 	}
-
 };
 
 const addUrlSection = (id, value) => {
-	const url = new URL(value);
 	const content = `<div id="${id}" class="alert alert-success alert-dismissible fade show" role="alert"><span class="url-regex">${url.origin}</span/><button type="button" class="btn-close delete-domain" data-bs-dismiss="alert" aria-label="Close"></button></div>`;
-	//const content = '<div id="' + id + '" class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><span class="url-regex">' + url.origin + '<span/></div>';
 	$('.list-url .mCSB_container').prepend(content);
 };
 
